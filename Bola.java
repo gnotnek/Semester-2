b3ec26d28c3831bf00fa689d7a1b02ccc6de0139
gnotnek <56432757+gnotnek@users.noreply.github.com>
2020-02-19 17:35:00 +0700

    bola bola
    tugas pak indra

--- Bola.java
+++ Bola.java
@@ -3,9 +3,26 @@ import java.util.Scanner;
 class Bola{
     Scanner ipt = new Scanner(System.in);
     double radius;
-
     public Bola(){
         System.out.println("Masukan radius/jari-jari: ");
         radius = ipt.nextDouble();
+    }
+    private double luasP(){
+        return 4*Math.PI*radius*radius;
+    }
+
+    private double volume(){
+        return (4/3)*Math.PI*radius*radius*radius;
+    }
+
+    public static void main(String[] args){
+
+        System.out.println("Bola!");
+        Bola bola1 = new Bola();
+
+        System.out.println("Luas permukaan = "+ bola1.luasP()+" cm^2");
+        System.out.println("Volume Bola = "+ bola1.volume()+" cm^3");
+
+
     }
 }
